67fb31a50159beb6c08613935e440ba667acd3a4
seddaluca <selucase1997@gmail.com>
2019-09-20 18:17:21 +0200

    Inizio flessibilità

--- app.py
+++ app.py
@@ -47,13 +47,13 @@ firebaseList = {
     }, {
         1: {"id": 1, "take": 3, "calories": 185, "name": "Hard Boiled Eggs"}
     }, {
-        2: {"id": 3, "take": 3, "calories": 200, "name": "Grilled Pork Chops"}
+        2: {"id": 2, "take": 3, "calories": 200, "name": "Grilled Pork Chops"}
     }, {
-        3: {"id": 4, "take": 3, "calories": 220, "name": "Paprika Chicken Legs"}
+        3: {"id": 3, "take": 3, "calories": 220, "name": "Paprika Chicken Legs"}
     }, {
-        4: {"id": 5, "take": 3, "calories": 250, "name": "Sea Bass in Salt Crust"}
+        4: {"id": 4, "take": 3, "calories": 250, "name": "Sea Bass in Salt Crust"}
     }, {
-        5: {"id": 6, "take": 3, "calories": 270, "name": "Pan-Fried Chicken"}
+        5: {"id": 5, "take": 3, "calories": 270, "name": "Pan-Fried Chicken"}
     }],
 
     "side dishes": [{
@@ -63,6 +63,9 @@ firebaseList = {
     }]
 }
 #endregion
+
+responseList = ['I suggest you ',
+                    'I recommend you ']
 
 list = {'meal': 'meal',
             'single dish': ['first',
@@ -100,19 +103,116 @@ def results():
 def webhook():
     return make_response(jsonify(results())) # return response
 
+def cleanLast():
+    side = ['First.txt',
+            'Second.txt',
+            'Side.txt',
+            'Fruit.txt']
+
+    for j in range(0,4):
+        try: # file esiste
+            try: # file esiste e contiene minimo una riga
+                dataset = pd.read_csv(str(date.isocalendar(date.today())[1])+side[j], sep=",", error_bad_lines=False)  # Leggo tutto il file
+
+                for i in range(0, len(dataset.index)):
+                    dataset.iloc[i]["Last"] = 0
+
+                dataset.to_csv(index=False, path_or_buf=str(date.isocalendar(date.today())[1])+side[j])
+
+            except pd.errors.EmptyDataError: # eccezione nel caso il file sia vuoto
+                print()
+        except FileNotFoundError: # eccezione nel caso non venga trovato il file
+            print()
+
 # catResponse(response, str(date.isocalendar(date.today())[1])+'First.txt', listFirstDishes, elementFirstDishes)
-def catResponse(response, file, list, element):
-    value = manage(file, list, element)
+def catResponse(new, response, file, list, element):
+    if new:
+        value = manage(file, list, element)
 
-    if value == (None, True):
-        return response + element.get("name") + ' (' + str(element.get("calories")) + ' kcal)'
+        if value == (None, True):
+            return response + element.get("name") + ' (' + str(element.get("calories")) + ' kcal)'
 
-    elif value == (None, False):
-        print("error")
+        elif value == (None, False):
+            print("error")
+
+        else:
+            element = list.__getitem__(value[0]).get(value[0]) # prendo il primo valore della coppia -> value[0]
+            return response + element.get("name") + ' (' + str(element.get("calories")) + ' kcal)'
 
     else:
-        element = list.__getitem__(value[0]).get(value[0])
+        value = checkChange(file, list)
+
+        print("Value: " + str(value[0]))
+
+        element = list.__getitem__(value[0]).get(value[0]) # prendo il primo valore della coppia -> value[0]
+
+        print("Element:" + str(element))
+
         return response + element.get("name") + ' (' + str(element.get("calories")) + ' kcal)'
+
+def checkChange(file, list):
+    dataset = pd.read_csv(file, sep=",", error_bad_lines=False)  # Leggo tutto il file
+
+    old = dataset.loc[dataset["Last"].idxmax()]
+
+    obj = list.__getitem__(0).get(0)  # da modificare
+
+    print(str(list))
+
+    print(str(old))
+
+    if old.loc["Last"] == 1:
+        if len(list) > len(dataset.index):
+            lenList = len(list) - 1
+
+            while True:  # do-while: restituisco un prodotto diverso dal precedente e non ancora proposto
+                value = random.randint(0, lenList)
+
+                if value not in dataset["ID"].to_list():
+                    break
+
+            for i in range(0, len(list)):  # prelevo l'elemento dalla lista
+                if value == list[i].__getitem__(i).get('id'):
+                    obj = list[i].__getitem__(i)
+
+            dataset.loc[dataset["Last"].idxmax()]["Take"] += 1
+            dataset.loc[dataset["Last"].idxmax()]["Last"] = 0
+
+            dataset = dataset.append(pd.DataFrame({"ID": [obj.get("id")],
+                                                   "Take": [(obj.get("take")-1)],
+                                                   "Last": [1]}), ignore_index=True)
+
+            dataset.to_csv(index=False, path_or_buf=file)
+
+            print("Sic: " + str(obj.get("id")))
+
+            return (obj.get("id"), False)
+
+        elif len(list) == len(dataset):
+            lenList = len(list) - 1
+
+            while True:  # do-while: restituisco un prodotto diverso dal precedente e non ancora proposto
+                value = random.randint(0, lenList)
+
+                if value != old.loc["ID"]:
+                    break
+
+            dataset.loc[dataset["Last"].idxmax()]["Take"] += 1
+            dataset.loc[dataset["Last"].idxmax()]["Last"] = 0
+
+            for i in range(0, len(dataset.index)):
+                if value == dataset.iloc[i]["ID"]:
+                    dataset.iloc[i]["Take"] -= 1
+                    dataset.iloc[i]["Last"] = 1
+
+                    obj = dataset.iloc[i]["ID"]
+
+            dataset.to_csv(index=False, path_or_buf=file)
+
+
+            return (int(obj), False)
+
+    return (None, False)
 
 # function for manage dataset
 def manage(file, list, obj):
@@ -120,12 +220,14 @@ def manage(file, list, obj):
         try: # file esiste e contiene minimo una riga
             dataset = pd.read_csv(file, sep=",", error_bad_lines=False)  # Leggo tutto il file
 
+            '''
             for i in range(0, len(dataset.index)):
                 dataset.iloc[i]["Last"] = 0
+            '''
 
             if obj.get("id") not in dataset["ID"].to_list(): # alimento non ancora mangiato nella settimana x
                 dataset = dataset.append(pd.DataFrame({"ID": [obj.get("id")],
-                                                       "Take": [obj.get("take")],
+                                                       "Take": [obj.get("take")-1],
                                                        "Last": [1]}), ignore_index=True)
 
                 dataset.to_csv(index=False, path_or_buf=file)
@@ -176,7 +278,7 @@ def manage(file, list, obj):
                                     obj = list[i].__getitem__(i)
 
                             dataset = dataset.append(pd.DataFrame({"ID": [obj.get("id")],
-                                                                   "Take": [obj.get("take")],
+                                                                   "Take": [obj.get("take")-1],
                                                                    "Last": [1]}), ignore_index=True)
 
                             dataset.to_csv(index=False, path_or_buf=file)
@@ -184,24 +286,20 @@ def manage(file, list, obj):
                             return (obj.get("id"), False)
 
         except pd.errors.EmptyDataError: # eccezione nel caso il file sia vuoto
-            dataset = pd.DataFrame(pd.DataFrame({"ID": [obj.get("id")], "Take": [obj.get("take")], "Last": [1]}))
+            dataset = pd.DataFrame(pd.DataFrame({"ID": [obj.get("id")], "Take": [obj.get("take")-1], "Last": [1]}))
 
             dataset.to_csv(index=False, path_or_buf=file)
     except FileNotFoundError: # eccezione nel caso non venga trovato il file
-            dataset = pd.DataFrame(pd.DataFrame({"ID": [obj.get("id")], "Take": [obj.get("take")], "Last": [1]}))
+            dataset = pd.DataFrame(pd.DataFrame({"ID": [obj.get("id")], "Take": [obj.get("take")-1], "Last": [1]}))
 
             dataset.to_csv(index=False, path_or_buf=file)
 
     return (None, True)
 
 def response(action):
-
-    responseList = ['I suggest you ',
-                    'I recommend you ']
-
-    response = responseList[random.randint(0, 1)] # imposto la risposta di default
-
     typeOfMeal = action.get('parameters').get('TypeOfMeal')
+
+    response = responseList[random.randint(0, 1)]  # imposto la risposta di default
 
     listFirstDishes = firebaseList.get('first dishes')
     listSecondDishes = firebaseList.get('second dishes')
@@ -223,45 +321,55 @@ def response(action):
     elementSideDishes = listSideDishes.__getitem__(sideDishes).get(sideDishes)
     elementFruit = listFruit.__getitem__(fruit).get(fruit)
 
+    cleanLast()
+
     if len(typeOfMeal) == 1:
         if list.get('meal') in typeOfMeal:
-            response = catResponse(response,
+            response = catResponse(True,
+                                   response,
                                    str(date.isocalendar(date.today())[1]) + 'First.txt',
                                    listFirstDishes,
                                    elementFirstDishes) + ', '
-            response = catResponse(response,
+            response = catResponse(True,
+                                   response,
                                    str(date.isocalendar(date.today())[1]) + 'Second.txt',
                                    listSecondDishes,
                                    elementSecondDishes) + ', '
-            response = catResponse(response,
+            response = catResponse(True,
+                                   response,
                                    str(date.isocalendar(date.today())[1]) + 'Side.txt',
                                    listSideDishes,
                                    elementSideDishes) + ' and '
-            response = catResponse(response,
+            response = catResponse(True,
+                                   response,
                                    str(date.isocalendar(date.today())[1]) + 'Fruit.txt',
                                    listFruit,
                                    elementFruit)
 
         if list.get('single dish')[0] in typeOfMeal:
-            response = catResponse(response,
+            response = catResponse(True,
+                                   response,
                                    str(date.isocalendar(date.today())[1]) + 'First.txt',
                                    listFirstDishes,
                                    elementFirstDishes)
 
         if list.get('single dish')[1] in typeOfMeal:
-            response = catResponse(response,
+            response = catResponse(True,
+                                   response,
                                    str(date.isocalendar(date.today())[1]) + 'Second.txt',
                                    listSecondDishes,
                                    elementSecondDishes)
 
         if list.get('single dish')[2] in typeOfMeal:
-            response = catResponse(response,
+            response = catResponse(True,
+                                   response,
                                    str(date.isocalendar(date.today())[1]) + 'Side.txt',
                                    listSideDishes,
                                    elementSideDishes)
 
         if list.get('single dish')[3] in typeOfMeal:
-            response = catResponse(response,
+            response = catResponse(True,
+                                   response,
                                    str(date.isocalendar(date.today())[1]) + 'Fruit.txt',
                                    listFruit,
                                    elementFruit)
@@ -269,25 +377,29 @@ def response(action):
     if len(typeOfMeal) > 1:
         for i in range(0, len(typeOfMeal)):
             if list.get('single dish')[0] in typeOfMeal[i]:
-                response = catResponse(response,
+                response = catResponse(True,
+                                       response,
                                        str(date.isocalendar(date.today())[1]) + 'First.txt',
                                        listFirstDishes,
                                        elementFirstDishes)
 
             if list.get('single dish')[1] in typeOfMeal[i]:
-                response = catResponse(response,
+                response = catResponse(True,
+                                       response,
                                        str(date.isocalendar(date.today())[1]) + 'Second.txt',
                                        listSecondDishes,
                                        elementSecondDishes)
 
             if list.get('single dish')[2] in typeOfMeal[i]:
-                response = catResponse(response,
+                response = catResponse(True,
+                                       response,
                                        str(date.isocalendar(date.today())[1]) + 'Side.txt',
                                        listSideDishes,
                                        elementSideDishes)
 
             if list.get('single dish')[3] in typeOfMeal[i]:
-                response = catResponse(response,
+                response = catResponse(True,
+                                       response,
                                        str(date.isocalendar(date.today())[1]) + 'Fruit.txt',
                                        listFruit,
                                        elementFruit)
@@ -305,10 +417,46 @@ def changeFood(action):
 
     typeOfMeal = action.get('parameters').get('TypeOfMeal')
 
+    response = responseList[random.randint(0, 1)]  # imposto la risposta di default
+
+    listFirstDishes = firebaseList.get('first dishes')
+    listSecondDishes = firebaseList.get('second dishes')
+    listSideDishes = firebaseList.get('side dishes')
+    listFruit = firebaseList.get('fruit')
+
+    lenListFirstDishes = len(listFirstDishes) - 1
+    lenListSecondDishes = len(listSecondDishes) - 1
+    lenListSideDishes = len(listSideDishes) - 1
+    lenListFruit = len(listFruit) - 1
+
+    firstDishes = random.randint(0, lenListFirstDishes)
+    secondDishes = random.randint(0, lenListSecondDishes)
+    sideDishes = random.randint(0, lenListSideDishes)
+    fruit = random.randint(0, lenListFruit)
+
     if len(typeOfMeal) == 1:
         if list.get('meal') in typeOfMeal:
-            return "meal"
-
+            response = catResponse(False,
+                                   response,
+                                   str(date.isocalendar(date.today())[1]) + 'First.txt',
+                                   listFirstDishes,
+                                   None) + ', '
+            response = catResponse(False,
+                                   response,
+                                   str(date.isocalendar(date.today())[1]) + 'Second.txt',
+                                   listSecondDishes,
+                                   None) + ', '
+            response = catResponse(False,
+                                   response,
+                                   str(date.isocalendar(date.today())[1]) + 'Side.txt',
+                                   listSideDishes,
+                                   None) + ' and '
+            response = catResponse(False,
+                                   response,
+                                   str(date.isocalendar(date.today())[1]) + 'Fruit.txt',
+                                   listFruit,
+                                   None)
+        '''
         if list.get('single dish')[0] in typeOfMeal:
             return "single dish"
 
@@ -320,6 +468,8 @@ def changeFood(action):
 
         if list.get('single dish')[3] in typeOfMeal:
             return "fruit"
+        '''
+    return response
 
 # run the app
 if __name__ == '__main__':
